3f085cca588e87e8d96c815b4dc0dbefc9407abf
Elysia-Forever <zerotoone288@gmail.com>
2022-11-15 23:54:55 +0700

    Test

--- DemoBuoi3/src/demobuoi3/DemoBuoi3.java
+++ DemoBuoi3/src/demobuoi3/DemoBuoi3.java
@@ -14,7 +14,11 @@ public class DemoBuoi3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello ae");
+          System.out.println("Hello ae");
+            System.out.println("Hello ae");
+              System.out.println("Hello ae");
+        
     }
     
 }
